f19bf0405ec6803d8ab9999ff1c01e1105a5b318
nourallah <halloul.nourallah@gmail.com>
2024-11-22 15:34:39 +0100

    project architecture

---
+++ src/main/java/com/tekup/storage_system/StorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tekup.storage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekup/storage_system/StorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekup.storage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
